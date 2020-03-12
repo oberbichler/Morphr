d9b9f911e7c556e12207febb21119e83851bdad5
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2020-03-12 11:12:27 +0100

    Add module

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='morphr',
+    version='0.1.0',
+    packages=['morphr'],
+    author='Thomas Oberbichler',
+    author_email='thomas.oberbichler@tum.de',
+    requires=[
+        'anurbs',
+        'colorama',
+        'eqlib',
+        'hyperjet',
+        'json',
+        'meshio',
+        'numpy',
+        'pydantic',
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'morphr=morphr.cli:main'
+        ]
+    }
+)
