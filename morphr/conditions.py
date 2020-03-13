25f55b8d4cbc47cae221530e9bda2c07752eda72
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2020-03-13 16:05:18 +0100

    Add PointOnSurfaceSupport

--- morphr/conditions.py
+++ morphr/conditions.py
@@ -37,3 +37,37 @@ def evaluate_act_2(nodes, shape_functions, size, offset):
     xyz[2].g[offset+2:offset+nb_dofs:3] = shape_functions
 
     return xyz
+
+
+class PointOnSurfaceSupport(eq.Objective):
+    def __init__(self, nodes, shape_functions, target, weight):
+        eq.Objective.__init__(self)
+        self.nodes = nodes
+        self.shape_functions = shape_functions
+        self.target = target
+        self.weight = weight
+        variables = []
+        for node in nodes:
+            variables += [node.x, node.y, node.z]
+        self.variables = variables
+
+    @property
+    def act_location(self):
+        return evaluate_act(self.nodes, self.shape_functions[0])
+
+    def evaluate_act_2(self, index):
+        nb_dofs = len(self.nodes) * 3
+        return evaluate_act_2(self.nodes, self.shape_functions[index], nb_dofs, 0)
+
+    def compute(self, g, h):
+        x = self.evaluate_act_2(0)
+
+        delta = self.target - x
+
+        p = np.dot(delta, delta) * self.weight / 2
+
+        g[:] = p.g
+        h[:] = p.h
+        return p.f
+
+
