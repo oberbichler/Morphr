44e46d7818ac30b4e395af8bb07358fbcdf8cc8b
thomas <thomas.oberbichler@gmail.com>
2020-04-17 18:26:58 +0200

    Use click

--- morphr/cli.py
+++ morphr/cli.py
@@ -1,10 +1,16 @@
 from morphr.configuration import Configuration
+from morphr import __version__
+import click
 
 
-def main():
+@click.group(invoke_without_command=True)
+@click.version_option(__version__, prog_name='morphr')
+@click.pass_context
+def cli(ctx):
+    if ctx.invoked_subcommand is not None:
+        return
     configuration = Configuration.load('configuration.json')
     configuration.run()
 
-
 if __name__ == '__main__':
-    main()
+    cli()
