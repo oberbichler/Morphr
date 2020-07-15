8c5b49ee22e50399b4e3149152858b3dd6ab6da0
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2020-07-15 15:22:10 +0200

    Cleanup

--- morphr/tasks/apply_edge_coupling.py
+++ morphr/tasks/apply_edge_coupling.py
@@ -94,9 +94,6 @@ class ApplyEdgeCoupling(mo.Task):
 
                     nb_objectives += 1
 
-                    if self.debug:
-                        cad_model.add(an.Point3D(element.act_b), r'{"layer": "Debug/ApplyEdgeCoupling/RotationAxis"}')
-
                 if self.debug:
                     point_a = nurbs_surface_a.point_at(u_a, v_a)
                     point_b = nurbs_surface_b.point_at(u_b, v_b)
